5a83f2c5865b9b9068c7a13fe52c67a5e26bd24e
LArnoldt <github@lucas-arnoldt.de>
2021-02-02 17:27:15 +0100

    Exercise 2 finalized.

---
+++ exercises/e02/plot_aa_histogram.py
@@ -0,0 +1,78 @@
+import argparse
+import matplotlib.pyplot as plt
+from collections import Counter
+
+def import_fasta_file(fasta_file_string):
+    """Imports the fasta file.
+
+    Args:
+        fasta_file_string: directory of the fasta file with the sequences to be analysed
+
+    Returns:
+        fasta_file: Loaded fasta file
+    """
+
+    fasta_file = open(fasta_file_string, "r")
+    return fasta_file
+
+def aggregate_rows_in_fasta_file(fasta_file):
+    """Aggregates all rows in the fasta file.
+
+    Args:
+        fasta_file: Loaded fasta file
+
+    Returns:
+        aggregated_fastas: aggregated fasta sequences
+    """
+
+    aggregated_fastas = ""
+
+    for row in fasta_file:
+        if row[0] != ">":
+            row = row[:-1]
+            aggregated_fastas += row
+
+    return aggregated_fastas
+
+def count_amino_acid_prevalences_in_aggregated_fastas(aggregated_fastas):
+    """Function outputs the calculated prevalences of the amino acids and saves them to the CSV
+
+    Args:
+        aggregated_fastas: aggregated fasta sequences
+
+    Returns:
+        amino_acids_dict: dictionary (Counter type) of amino acids with prevalences
+    """
+
+    amino_acids_dict = Counter(aggregated_fastas)
+    return amino_acids_dict
+
+def plot_amino_acid_prevalences(amino_acids_dict):
+    """Function plots a histogram for the prevalences of the amino acids in the dictionary.
+
+    Args:
+        amino_acids_dict: dictionary with prevalences for single amino acids
+    """
+
+    prevalences_amino_acids = list(amino_acids_dict.values())
+    amino_acids = list(amino_acids_dict.keys())
+
+    plt.bar(amino_acids, prevalences_amino_acids, color="green")
+    plt.ylabel("Prevalences (x 10^6)")
+    plt.savefig("./exercises/e02/prevalence_distribution_amino_acids.png")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--fasta_file", help="FASTA file with the sequences to be analyzed. ", type=str)
+    args = parser.parse_args()
+    fasta_file_string = args.fasta_file
+
+    fasta_file = import_fasta_file(fasta_file_string)
+
+    aggregated_fastas = aggregate_rows_in_fasta_file(fasta_file)
+
+    amino_acids_dict = count_amino_acid_prevalences_in_aggregated_fastas(aggregated_fastas)
+
+    plot_amino_acid_prevalences(amino_acids_dict)
+
+    fasta_file.close()
