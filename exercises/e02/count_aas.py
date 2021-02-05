897c8dbb9fd839a949433d387a6eb752e11304f7
LArnoldt <github@lucas-arnoldt.de>
2021-02-06 00:29:50 +0100

    Some updates. More to come.

--- exercises/e02/count_aas.py
+++ exercises/e02/count_aas.py
@@ -23,16 +23,31 @@ def aggregate_rows_in_fasta_file(fasta_file):
 
     Returns:
         aggregated_fastas: aggregated fasta sequences
+        longest_fasta: length of the longest protein sequence
+        shortest_fasta: length of the shortest protein sequence
     """
 
     aggregated_fastas = ""
+    longest_fasta = 0
+    shortest_fasta = float('inf')
+    counter = 0
+    aggregated_fastas_protein = ""
 
     for row in fasta_file:
         if row[0] != ">":
             row = row[:-1]
             aggregated_fastas += row
+            counter += 1
+            aggregated_fastas_protein += row
+        if row[0] == ">":
+            counter = 0
+            if len(aggregated_fastas_protein) > longest_fasta:
+                longest_fasta = len(aggregated_fastas_protein)
+            if len(aggregated_fastas_protein) < shortest_fasta and len(aggregated_fastas_protein) > 0:
+                shortest_fasta = len(aggregated_fastas_protein)
+            aggregated_fastas_protein = ""
 
-    return aggregated_fastas
+    return aggregated_fastas, longest_fasta, shortest_fasta
 
 def count_amino_acid_prevalences_in_aggregated_fastas(aggregated_fastas):
     """Function outputs the calculated prevalences of the amino acids and saves them to the CSV
@@ -61,18 +76,70 @@ def output_amino_acid_prevalences_and_save_to_csv(amino_acids_dict):
             print(str(amino_acid) + ": " + str(amino_acids_dict[amino_acid]))
             aa_distribution_csv.writerow([str(amino_acid) + ", " + str(amino_acids_dict[amino_acid])])
 
+
+def import_amino_acid_properties_and_create_pi_dict(directory_amino_acid_properties):
+    '''Function imports a table with amino acid properties and extracts the hydropathy scores for certain bases.
+
+    Args:
+        directory_amino_acid_properties: directory of table with amino acid properties, e.g. hydropathy
+    Returns:
+        pi_dict: dict of pi scores for certain amino acids
+    '''
+
+    pi_dict = {}
+
+    with open(directory_amino_acid_properties, newline='') as csvfile:
+        amino_acid_properties = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for pos, row in enumerate(amino_acid_properties):
+            if pos > 0:
+                pi_dict[row[2]] = float(row[10])
+
+    return pi_dict
+
+def calculation_pi(amino_acids_dict, pi_dict):
+    '''Function calculates the pI of a sequence.
+
+    Args:
+        amino_acids_dict: dictionary (Counter type) of amino acids with prevalences
+        pi_dict: dict of pi scores for certain amino acids
+    Returns:
+        pi: pi score for a sequence
+    '''
+
+    pi = 0
+
+    for amino_acid, count in amino_acids_dict.items():
+        try:
+            pi += pi_dict[amino_acid] * count
+        except:
+            print("The amino acid " + amino_acid + " was not given in the file amino_acid_properties.csv. Therefore it is ignored for the futher calculation.")
+
+    pi = pi / sum(amino_acids_dict.values())
+
+    return pi
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--fasta_file", help="FASTA file with the sequences to be analyzed. ", type=str)
+    parser.add_argument("--directory_amino_acid_properties", help="Directory of CSV with amino acid properties",
+                        type=str, required=False)
     args = parser.parse_args()
     fasta_file_string = args.fasta_file
+    directory_amino_acid_properties = args.directory_amino_acid_properties
 
     fasta_file = import_fasta_file(fasta_file_string)
 
-    aggregated_fastas = aggregate_rows_in_fasta_file(fasta_file)
+    aggregated_fastas, longest_fasta, shortest_fasta = aggregate_rows_in_fasta_file(fasta_file)
 
     amino_acids_dict = count_amino_acid_prevalences_in_aggregated_fastas(aggregated_fastas)
 
     output_amino_acid_prevalences_and_save_to_csv(amino_acids_dict)
+
+    pi_dict = import_amino_acid_properties_and_create_pi_dict(directory_amino_acid_properties)
+    pi = calculation_pi(amino_acids_dict, pi_dict)
+
+    print("The total pI of the sequence is " + str(pi) + ".")
+    print("The longest FASTA in the inputted FASTA file has a length of " + str(longest_fasta) + ".")
+    print("The shortest FASTA in the inputted FASTA file has a length of " + str(shortest_fasta) + ".")
 
     fasta_file.close()
--- exercises/e04/e04.py
+++ exercises/e04/e04.py
@@ -2,22 +2,25 @@ import argparse
 import requests
 import plotly.express as px
 import plotly.io as pio
+
 pio.renderers.default = "browser"
 from pathlib import Path
 import csv
 from collections import deque
 import pandas as pd
 
+
 def uniprot_download_single_entry(acc_uniprot):
-   """Function downloads the corresponding .fasta file for a uniprot accession number and saves it.
+    """Function downloads the corresponding .fasta file for a uniprot accession number and saves it.
 
-   Args:
-       acc_uniprot: uniprot accession number
-   """
+    Args:
+        acc_uniprot: uniprot accession number
+    """
 
-   url_entry_uniprot = "https://www.uniprot.org/uniprot/" + acc_uniprot + ".fasta"
-   request_entry_uniprot = requests.get(url_entry_uniprot, allow_redirects=True)
-   open("./exercises/e03/" + acc_uniprot + ".fasta", 'wb').write(request_entry_uniprot.content)
+    url_entry_uniprot = "https://www.uniprot.org/uniprot/" + acc_uniprot + ".fasta"
+    request_entry_uniprot = requests.get(url_entry_uniprot, allow_redirects=True)
+    open("./exercises/e03/" + acc_uniprot + ".fasta", 'wb').write(request_entry_uniprot.content)
+
 
 def extract_fasta(fasta_file_string):
     '''Function extracs FASTA string from fasta file.
@@ -39,6 +42,7 @@ def extract_fasta(fasta_file_string):
 
     return fasta
 
+
 def import_amino_acid_properties_and_create_hydropathy_dict(directory_amino_acid_properties):
     '''Function imports a table with amino acid properties and extracts the hydropathy scores for certain bases.
 
@@ -47,7 +51,7 @@ def import_amino_acid_properties_and_create_hydropathy_dict(directory_amino_acid
     Returns:
         hydropathy_dict: dict of hydropathy scores for certain bases
     '''
-    
+
     hydropathy_dict = {}
 
     with open(directory_amino_acid_properties, newline='') as csvfile:
@@ -57,6 +61,7 @@ def import_amino_acid_properties_and_create_hydropathy_dict(directory_amino_acid
                 hydropathy_dict[row[2]] = float(row[11])
 
     return hydropathy_dict
+
 
 def create_hydropathy_value_list_for_fasta(fasta, hydropathy_dict, length_sliding_window):
     '''Function generates a list of hydropathy scores for the sequence making use of a sliding window.
@@ -79,7 +84,7 @@ def create_hydropathy_value_list_for_fasta(fasta, hydropathy_dict, length_slidin
     return hydropathy_sequence_list
     '''
 
-    #Version for Task D)
+    # Version for Task D)
 
     window = deque([], maxlen=length_sliding_window)
 
@@ -93,83 +98,43 @@ def create_hydropathy_value_list_for_fasta(fasta, hydropathy_dict, length_slidin
 
     return hydropathy_sequence_list
 
-def plot_hydropathy_list(hydropathy_sequence_list, acc_uniprot, length_sliding_window, topological_domains):
+
+def plot_hydropathy_list(hydropathy_sequence_list, acc_uniprot, length_sliding_window):
     ''' Function generates a pandas table with the sequence positions and the hydropathy_values and plots them in a bar chart.
 
     Args:
         hydropathy_sequence_list: hydropathy values for sequence positions
         acc_uniprot: uniprot accession number
         length_sliding_window: length of the sliding window used in the determination of the hydropathy
-        topological_domains: topological domains of the corresponding protein
     '''
 
     sequence_positions = []
     [sequence_positions.append(sequence_position) for sequence_position in range(0, len(hydropathy_sequence_list))]
 
     sequence_position_hydropathy_tuple = list(zip(sequence_positions, hydropathy_sequence_list))
-    sequence_position_hydropathy_df = pd.DataFrame(sequence_position_hydropathy_tuple, columns=['Sequence Position', 'Hydropathy'])
-    ######
-    #Insert topological topological_domains
-    ####
+    sequence_position_hydropathy_df = pd.DataFrame(sequence_position_hydropathy_tuple,
+                                                   columns=['Sequence Position', 'Hydropathy'])
 
-    hydropathy_plot = px.bar(sequence_position_hydropathy_df, x = 'Sequence Position', y = 'Hydropathy', title="Hydropathy List for Uniprot Acc: " + acc_uniprot + " with a sliding window of " + str(length_sliding_window))
+    hydropathy_plot = px.bar(sequence_position_hydropathy_df, x='Sequence Position', y='Hydropathy',
+                             title="Hydropathy List for Uniprot Acc: " + acc_uniprot + " with a sliding window of " + str(
+                                 length_sliding_window))
     hydropathy_plot.show()
-
-def extract_xml_and_extract_topological_domains(directory_xml_file):
-    '''Function imports the XML file of a protein and extracts all topological domains.
-
-    Args:
-        directory_xml_file: directory of the XML file for a corresponsing protein
-    Returns:
-        topological_domains: topological domains of the corresponding protein
-    '''
-
-    import xml.etree.ElementTree as ET
-
-    tree = ET.parse(directory_xml_file)
-    root = tree.getroot()
-
-    for child in root:
-        print(child)
-        for child2 in child:
-            #print("Tag")
-            #print(child2.tag)
-            #print("Attrib")
-            #print(child2.attrib)
-            try:
-                child2.attrib['type'] == "transmembrane_region"# or child2.attrib['type'] == "topological domain"
-                print(child2.attrib)
-                break
-            except:
-                continue
-
-    return topological_domains
-
-#####
-
-    import Bio
-
-    xml_file = Bio.SeqIO.UniprotIO.UniprotIterator(directory_xml_file)
-
-    for row in xml_file:
-        print(row)
-
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--fasta_file", help="FASTA file with the sequences to be analyzed.", type=str, default="NaN", required=False)
+    parser.add_argument("--fasta_file", help="FASTA file with the sequences to be analyzed.", type=str, default="NaN",
+                        required=False)
     parser.add_argument("--acc_uniprot", help="Uniprot accession number.", type=str, default="NaN", required=False)
     parser.add_argument("--length_sliding_window", help="Length of the sliding window.", type=int, required=True)
-    parser.add_argument("--directory_amino_acid_properties", help="Directory of CSV with amino acid properties", type=str, required=True)
-    parser.add_argument("--directory_xml_file", help="Directory of CSV with amino acid properties",type=str, required=True)
+    parser.add_argument("--directory_amino_acid_properties", help="Directory of CSV with amino acid properties",
+                        type=str, required=True)
     args = parser.parse_args()
 
     fasta_file_string = args.fasta_file
     acc_uniprot = args.acc_uniprot
     length_sliding_window = args.length_sliding_window
     directory_amino_acid_properties = args.directory_amino_acid_properties
-    directory_xml_file = args.directory_xml_file
 
     breaking = False
 
@@ -185,8 +150,6 @@ if __name__ == "__main__":
         breaking = True
 
     if breaking is not True:
-
         hydropathy_dict = import_amino_acid_properties_and_create_hydropathy_dict(directory_amino_acid_properties)
         hydropathy_sequence_list = create_hydropathy_value_list_for_fasta(fasta, hydropathy_dict, length_sliding_window)
-        topological_domains = extract_xml_and_extract_topological_domains(directory_xml_file)
-        plot_hydropathy_list(hydropathy_sequence_list, acc_uniprot.stem, length_sliding_window, topological_domains)
+        plot_hydropathy_list(hydropathy_sequence_list, acc_uniprot.stem, length_sliding_window)
